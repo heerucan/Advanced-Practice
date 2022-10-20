2972fd56f469a2891b7f1dd3b02e1dd68e6b164e
heerucan <jwyandkrh@naver.com>
2022-10-20 22:15:03 +0900

    Add: SearchVC 레이아웃 구현 #3

--- SSAC-Advanced-Project/SSAC-Advanced-Project/Source/Search/Controller/SearchViewController.swift
+++ SSAC-Advanced-Project/SSAC-Advanced-Project/Source/Search/Controller/SearchViewController.swift
@@ -11,19 +11,69 @@ final class SearchViewController: BaseViewController {
     
     // MARK: - Property
     
-//    let collectionView
+    private let searchView = SearchView()
+    
+    private var dataSource: UICollectionViewDiffableDataSource<Int, String>!
     
     // MARK: - LifeCycle
     
+    override func loadView() {
+        self.view = searchView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureDataSource()
     }
     
     // MARK: - Configure UI & Layout
 
+    override func setupDelegate() {
+        searchView.setupDelegate(self)
+    }
+    
+    // MARK: - Bind
+    
+    override func bindData() {
+        print(#function)
+    }
     
     // MARK: - Custom Method
     
     
     // MARK: - @objc
+}
+
+// MARK: - SearchBarDelegate
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
+}
+
+// MARK: - DiffableDataSource
+
+extension SearchViewController {
+    func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<SearchCollectionViewCell, String> { cell, indexPath, itemIdentifier in
+            // String > URL > Data > Image 방식
+            
+//            cell.imageView.image.
+            cell.backgroundColor = .red
+            cell.layer.borderColor = UIColor.black.cgColor
+            cell.layer.borderWidth = 2
+        }
+    
+        dataSource = UICollectionViewDiffableDataSource(collectionView: searchView.collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            let cell = collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration, for: indexPath, item: itemIdentifier)
+            return cell
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Int, String>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(["안녕", "안d녕", "안ss녕", "안a녕", "안g녕", "안s녕", "안gh녕"])
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
 }
--- SSAC-Advanced-Project/SSAC-Advanced-Project/Source/Search/View/SearchView.swift
+++ SSAC-Advanced-Project/SSAC-Advanced-Project/Source/Search/View/SearchView.swift
@@ -9,10 +9,81 @@ import UIKit
 
 final class SearchView: BaseView {
     
+    // MARK: - Property
+    
+    let searchBar = UISearchBar().then {
+        $0.placeholder = "검색하고 싶은 사진을 영어로 입력해주세요"
+    }
+    
+    lazy var collectionView = UICollectionView(
+        frame: .zero, collectionViewLayout: createLayout())
+    
     // MARK: - Initializer
     
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
+
+    // MARK: - Configure UI
     
+    override func configureLayout() {
+        addSubview(searchBar)
+        addSubview(collectionView)
+        
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide.snp.top)
+            make.directionalHorizontalEdges.equalToSuperview()
+            make.height.equalTo(50)
+        }
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.directionalHorizontalEdges.equalToSuperview()
+            make.bottom.equalTo(self.safeAreaLayoutGuide)
+        }
+    }
+    
+    // MARK: - Custom Method
+    
+    func setupDelegate(_ delegate: UISearchBarDelegate) {
+        searchBar.delegate = delegate
+    }
+}
+
+// MARK: - Compositional Layout
+
+extension SearchView {
+    private func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (sectionIndex, NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection?  in
+            
+            let itemSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1/2),
+                heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalWidth(1/2))
+            
+            let group = NSCollectionLayoutGroup.horizontal(
+                layoutSize: groupSize,
+                subitem: item, count: 2)
+            group.interItemSpacing = .fixed(5)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            let spacing = 5.0
+            section.interGroupSpacing = spacing
+            section.contentInsets = NSDirectionalEdgeInsets(
+                top: spacing, leading: spacing,
+                bottom: spacing, trailing: spacing)
+            return section
+        }
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        let layout = createCompositionalLayout()
+        layout.configuration = configuration
+        return layout
+    }
 }
