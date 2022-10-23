4e7562b705e37639f6623e195f714c6d463422a2
heerucan <iwpow0307@gmail.com>
2022-10-24 01:53:18 +0900

    Add: SearchViewModel에 requestSearchUser 서버 통신 후 데이터 받아서 SearchVC으로 전달 #5

--- SSAC-Advanced-Project/SSAC-Advanced-Project/Source/Search/ViewModel/SearchViewModel.swift
+++ SSAC-Advanced-Project/SSAC-Advanced-Project/Source/Search/ViewModel/SearchViewModel.swift
@@ -2,11 +2,20 @@
 //  SearchViewModel.swift
 //  SSAC-Advanced-Project
 //
-//  Created by heerucan on 2022/10/20.
+//  Created by heerucan on 2022/10/24.
 //
 
 import Foundation
 
 final class SearchViewModel {
     
+    var userList: CObservable<SearchUser> = CObservable(SearchUser(total: 0, totalPages: 0, results: []))
+    
+    func requestSearchUser(query: String) {
+        PhotoAPIManager.shared.getSearchUser(query: query) { [weak self] (user, status, error) in
+            guard let user = user,
+                  let self = self else { return }
+            self.userList.value = user
+        }
+    }
 }
