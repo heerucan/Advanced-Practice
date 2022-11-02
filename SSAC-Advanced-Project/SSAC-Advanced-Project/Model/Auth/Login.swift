34622e4cc1554871385b746e88ea6d6eb6aef66c
heerucan <iwpow0307@gmail.com>
2022-11-02 23:23:47 +0900

    Add: Auth Data Model #16

--- SSAC-Advanced-Project/SSAC-Advanced-Project/Model/Auth/Login.swift
+++ SSAC-Advanced-Project/SSAC-Advanced-Project/Model/Auth/Login.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+// MARK: - Login
+
+struct Login: Codable {
+    let token: String
+}
--- SSAC-Advanced-Project/SSAC-Advanced-Project/Model/Auth/Profile.swift
+++ SSAC-Advanced-Project/SSAC-Advanced-Project/Model/Auth/Profile.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+// MARK: - Profile
+
+struct Profile: Codable {
+    let auth: Auth
+}
+
+// MARK: - Auth
+
+struct Auth: Codable {
+    let photo, email, username: String
+
+    enum CodingKeys: String, CodingKey {
+        case photo, email, username
+    }
+}
