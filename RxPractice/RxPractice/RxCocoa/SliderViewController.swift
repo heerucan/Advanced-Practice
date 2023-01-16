dcebbbca6e92a121c94b106e87e544386b2d89c0
heerucan <iwpow0307@gmail.com>
2023-01-16 20:25:17 +0900

    Add: CombineLatest 적용해서 슬라이더 움직임에 따라 뷰 색상 변경

--- RxPractice/RxPractice/RxCocoa/SliderViewController.swift
+++ RxPractice/RxPractice/RxCocoa/SliderViewController.swift
@@ -7,23 +7,72 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+import SnapKit
+
 class SliderViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    let bag = DisposeBag()
+    
+    @IBOutlet weak var backgroundColorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redComponentLabel: UILabel!
+    @IBOutlet weak var greenComponentLabel: UILabel!
+    @IBOutlet weak var blueComponentLabel: UILabel!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    private func updateComponentLabel() {
+        redComponentLabel.text = "\(Int(redSlider.value))"
+        greenComponentLabel.text = "\(Int(greenSlider.value))"
+        blueComponentLabel.text = "\(Int(blueSlider.value))"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        redSlider.rx.value
+            .map { "\(Int($0))" }
+            .bind(to: redComponentLabel.rx.text)
+            .disposed(by: bag)
+        
+        greenSlider.rx.value
+            .map { "\(Int($0))" }
+            .bind(to: greenComponentLabel.rx.text)
+            .disposed(by: bag)
+        
+        blueSlider.rx.value
+            .map { "\(Int($0))" }
+            .bind(to: blueComponentLabel.rx.text)
+            .disposed(by: bag)
+        
+        
+        let sliderObservable = Observable.combineLatest([redSlider.rx.value,
+                                                         greenSlider.rx.value,
+                                                         blueSlider.rx.value])
+        
+        sliderObservable
+            .map { UIColor(red: CGFloat($0[0]) / 255,
+                           green: CGFloat($0[1]) / 255,
+                           blue: CGFloat($0[2]) / 255,
+                           alpha: 1) }
+            .bind(to: backgroundColorView.rx.backgroundColor)
+            .disposed(by: bag)
+        
+        resetButton.rx.tap
+            .withUnretained(self)
+            .subscribe { _ in
+                self.backgroundColorView.backgroundColor = UIColor.black
+                self.redSlider.value = 0
+                self.greenSlider.value = 0
+                self.blueSlider.value = 0
+                self.updateComponentLabel()
+            }
+            .disposed(by: bag)
     }
-    */
-
 }
