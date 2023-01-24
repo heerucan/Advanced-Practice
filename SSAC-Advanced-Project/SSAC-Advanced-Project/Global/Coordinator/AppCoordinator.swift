d6a6506960b50ead1aaad5ddb0a2909c8777bc8b
heerucan <iwpow0307@gmail.com>
2023-01-25 00:11:12 +0900

    Add: Coordinator, AppCoordinator 추가 #21

--- SSAC-Advanced-Project/SSAC-Advanced-Project/Global/Coordinator/AppCoordinator.swift
+++ SSAC-Advanced-Project/SSAC-Advanced-Project/Global/Coordinator/AppCoordinator.swift
@@ -5,4 +5,33 @@
 //  Created by heerucan on 2023/01/24.
 //
 
-import Foundation
+import UIKit
+
+class AppCoordinator: Coordinator {
+    var childrenCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    // SceneDelegate에서 인자값으로 받은 값
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let loginViewModel = LoginViewModel()
+        loginViewModel.coordinator = self // LoginViewModel의 coordinator를 AppCoordinator로 설정
+        
+        let loginViewController = LoginViewController(view: LoginView(), viewModel: loginViewModel)
+        loginViewController.loginViewModel = loginViewModel // LoginViewController의 viewModel도 LoginViewModel로 설정
+        
+        navigationController.pushViewController(loginViewController, animated: false)
+    }
+    
+    func goToHomeVC() {
+        let homeViewModel = HomeViewModel()
+        homeViewModel.coordinator = self
+        
+        let homeViewController = HomeViewController(view: HomeView(), viewModel: homeViewModel)
+        homeViewController.homeViewModel = homeViewModel
+        navigationController.pushViewController(homeViewController, animated: true)
+    }
+}
--- SSAC-Advanced-Project/SSAC-Advanced-Project/Global/Coordinator/Coordinator.swift
+++ SSAC-Advanced-Project/SSAC-Advanced-Project/Global/Coordinator/Coordinator.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 protocol Coordinator {
-    var children: [Coordinator] { get set }
+    var childrenCoordinators: [Coordinator] { get set }
     var navigationController: UINavigationController { get set }
     
     func start()
