696bd074d3c77a9f9a76aab52ac5201ed924b7e6
heerucan <iwpow0307@gmail.com>
2023-03-06 13:31:44 +0900

    Add: 메모추가 alertVC #22

--- MVPPractice/MVPPractice/Application/Memo/MemoViewController.swift
+++ MVPPractice/MVPPractice/Application/Memo/MemoViewController.swift
@@ -20,7 +20,15 @@ final class MemoViewController: UIViewController, MemoViewProtocol {
     private var dataSource: UICollectionViewDiffableDataSource<Section, Memo>! = nil
     private var snapshot = NSDiffableDataSourceSnapshot<Section, Memo>()
     
-    private lazy var memoCollectionView = UICollectionView(frame: .zero, collectionViewLayout: self.createLayout())
+    private lazy var memoCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: self.createLayout())
+    
+    private lazy var addButton = UIBarButtonItem(
+        image: UIImage(systemName: "plus"),
+        style: .plain,
+        target: self,
+        action: #selector(addButtonClicked(sender:)))
     
     // MARK: - LifeCycle
     
@@ -35,7 +43,8 @@ final class MemoViewController: UIViewController, MemoViewProtocol {
         view.backgroundColor = .white
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "메모리스트"
-        memoCollectionView.backgroundColor = .white
+        navigationItem.rightBarButtonItem = addButton
+        memoCollectionView.backgroundColor = .systemBackground
         configureLayout()
         configureDataSource()
     }
@@ -62,7 +71,24 @@ final class MemoViewController: UIViewController, MemoViewProtocol {
     func updateMemo(index: Int, content: String) {
         
     }
-    
+                                                   
+    // MARK: - @objc
+    @objc func addButtonClicked(sender: UIBarButtonItem) {
+        let alertVC = UIAlertController(title: "메모추가", message: "간단한 메모를 작성하세요", preferredStyle: .alert)
+        alertVC.addTextField { textField in
+            textField.placeholder = "ex. 알고리즘 풀기"
+            textField.textColor = .black
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel) { _ in
+            self.dismiss(animated: false)
+        }
+        let ok = UIAlertAction(title: "추가", style: .default) { _ in
+            print("메모추가완료")
+        }
+        alertVC.addAction(cancel)
+        alertVC.addAction(ok)
+        self.present(alertVC, animated: true)
+    }
 }
 
 // MARK: - Compositional Layout & DiffableDataSource
